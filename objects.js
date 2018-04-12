a8d4ed50df0c103c4ba5f4e2a093832592ab412f
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-12 21:00:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = new Object({ artist: 'song' })
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({},artistName, songTitle)
+
+function updatePlaylist(artistName, songTitle) {
+  playlist[artistname] = songTitle
+  return playlist 
 }
