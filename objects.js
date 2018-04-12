598c9d2c815ed4b94588a9399a1bc2e3e4a3b0aa
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-12 20:53:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,8 @@
 var playlist = new Object({ artist: 'song' })
 
+updatePlaylist(playlist, artistName, songTitle)
 
+function udateplaylist(playlist, artistName, songTitle) {
+  obj[artistname] = songTitle
+  return obj
+}
